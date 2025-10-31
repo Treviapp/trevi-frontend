a752a21552cc1e85912ade112d6a5688b83a6798
Amanda Hughes <amanda@trevi-app.com>
2025-10-31 10:15:25 +0000

    Prepare live payment test build

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import { StripeProvider } from '@stripe/stripe-react-native';
 import { registerRootComponent } from 'expo';
 import Routes from './src/navigation/Routes';
 
-registerRootComponent(Routes);
+export default function App() {
+  return (
+    <StripeProvider publishableKey="pk_live_51RXKLjIZnBW7XxVHInY17LFasEoyTuZB88ytB4LLScE7L113h1Qzgk19T2R9ROiNQ8TBUYvBIJ0yUPkLVSM9LuGB00EFXISZp1">
+      <Routes />
+    </StripeProvider>
+  );
+}
+
+registerRootComponent(App);
